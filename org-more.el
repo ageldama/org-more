684ed4447881adfce7d84a18ac1f30300894f1c8
jonghyouk yun <ageldama@gmail.com>
2020-11-04 03:26:07 +0900

    ...

--- org-more.el
+++ org-more.el
@@ -1,11 +1,12 @@
-(defun org-more-list-of-tags (base-dir tags)
+(defun org-more-list-of-tags* (base-dir tags)
   (cl-flet ((mkdttms () (format-time-string "%Y-%m-%dT%T%z")))
     (let* ((dttm (mkdttms))
            (buf-name (format "*** Org-More tag search: %s // %s @@ %s ***"
                              tags base-dir dttm))
            (buf (generate-new-buffer buf-name))
            (cmd (format "org-more.pl find %s %s"
-                        base-dir tags)))
+                        base-dir tags))
+           (exit-code nil))
       (switch-to-buffer-other-window buf)    
       (with-current-buffer buf
         (insert "-*- mode: org; -*-\n")
@@ -17,12 +18,20 @@
         (insert (format "base-dir: %s\n" base-dir))
         (insert (format "started-at: %s\n" dttm))
         (insert "\n")
-        (shell-command cmd buf buf)
-        (insert (format "\nended-at: %s\n" (mkdttms) ))
+        (setf exit-code
+              (call-process-shell-command cmd nil buf buf))
+        (insert (format "\nended-at: %s\n" (mkdttms)))
+        (insert (format "exited-with: %s\n" exit-code))
         ;;
         (read-only-mode)))))
 
-(org-more-list-of-tags "/home/jhyun/P/ageldama-github-io-hugo/content/posts" "rxjs")
+(defun org-more-list-of-tags ()
+  (interactive)
+  (let ((base-dir (read-directory-name "Dir? "))
+        (tags (read-string "Enter tags: ")))
+    (org-more-list-of-tags* base-dir tags)))
 
 
-  
+
+
+;;; TODO interactive ver?
